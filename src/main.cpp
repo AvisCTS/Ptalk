da3f859eac9e1648570f78560671111ea3db2c47
trungnguyen278 <trung2782002@gmail.com>
2025-12-16 22:37:33 +0700

    Add DeviceProfile setup in app_main for initialization

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 
 
-#include <stdio.h>
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_log.h"
 #include "AppController.hpp"
+#include "DeviceProfile.hpp"
 
 static const char *TAG = "MAIN_TEST";
 
@@ -22,6 +23,12 @@ extern "C" void app_main(void)
 
     // Khởi tạo AppController
     auto& app = AppController::instance();
+
+    if (!DeviceProfile::setup(app)) {
+        ESP_LOGE(TAG, "DeviceProfile setup failed");
+        return;
+    }
+
     if (!app.init()) {
         ESP_LOGE(TAG, "AppController init failed");
         return;
